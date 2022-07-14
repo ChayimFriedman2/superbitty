67f0424f89c8096b80ee5c4b981e921b1beb04c6
Chayim Refael Friedman <chayimfr@gmail.com>
2022-07-14 05:11:12 +0300

    Generalize the docs of `BitFieldCompatible` to allow for structs too

--- superbitty/src/lib.rs
+++ superbitty/src/lib.rs
@@ -3,7 +3,7 @@
 /// A bitfield struct.
 ///
 /// All fields must implement [`BitFieldCompatible`] and their ranges must be non-overlapping
-/// and big enough to hold the enums.
+/// and big enough to hold the bitfields.
 ///
 /// ```
 /// # use superbitty::{bitfields, BitFieldCompatible};
@@ -59,52 +59,64 @@ pub use superbitty_macros::bitfields;
 /// ```
 pub use superbitty_macros::BitFieldCompatible;
 
-/// An enum that can be used as a bitfield. This is usually [derived].
+/// An enum or struct that can be used as a bitfield. This is usually [derived] for
+/// enums and implemented by the [`bit_field_compatible`] attribute for structs.
 ///
 /// # Safety
 ///
-///   - An enum implementing this trait must not carry payload.
+///   - An enum implementing this trait must not carry payload in any of its variants.
 ///   - The [`SHIFT`] and [`BITS_LEN`] values must be correct.
-///   - Variants must not have negative discriminants.
-///   - Enums must be inhabited, that is, have at least one variant.
-///   - [`into_raw()`] must return a valid discriminant (something you
-///     can feed back to [`from_raw()`]).
+///   - The type must be [inhabited].
+///   - [`into_raw()`] must return a valid value (something you can feed back
+///     to [`from_raw()`]).
 ///
 /// [derived]: macro@BitFieldCompatible
 /// [`SHIFT`]: BitFieldCompatible::SHIFT
 /// [`BITS_LEN`]: BitFieldCompatible::BITS_LEN
+/// [inhabited]: https://rustc-dev-guide.rust-lang.org/appendix/glossary.html?highlight=uninhabited#glossary
 /// [`into_raw()`]: BitFieldCompatible::into_raw
 /// [`from_raw()`]: BitFieldCompatible::from_raw
 pub unsafe trait BitFieldCompatible: Copy {
-    /// The number we need to left-shift with to reach a valid variant from a compressed
+    /// The number we need to left-shift with to reach a valid value from a compressed
     /// value which has all trailing zeros trimmed.
     ///
-    /// For example, for the discriminants `0b01000`, `0b10000` and `0b11000`, the
-    /// value of [`BITS_LEN`] will be 2 and the value of `SHIFT` will be 3, because
-    /// we need to left-shift three times to go from `0b01`, `0b10` and `0b11` to
-    /// the corresponding variants.
+    /// For example, in an enum with the discriminants `0b01000`, `0b10000` and `0b11000`,
+    /// the value of [`BITS_LEN`] will be 2 and the value of `SHIFT` will be 3, because
+    /// we need to left-shift three times to go from `0b01`, `0b10` and `0b11` to the
+    /// corresponding variants.
     ///
     /// [`BITS_LEN`]: BitFieldCompatible::BITS_LEN
     const SHIFT: u32;
 
-    /// The minimum number of bits this enum can occupy.
+    /// The minimum number of bits required to represent any bit pattern that is valid
+    /// for this type.
     ///
-    /// If there is only one variant, and it is not zero, this must be 1 even though
-    /// technically zero bits suffice. If the variant is zero this can be zero.
+    /// Note that enums with only one variant may still have this value greater than zero
+    /// if the discriminant of this variant is not zero, for example if the discriminant is
+    /// `0b11` it needs two bits to be stored.
+    ///
+    /// You can play with this value as long as you keep this rule of thumb: if we call
+    /// [`into_raw()`] and truncate the bits left after applying this value (that is,
+    /// perform the operation `into_raw(v) & (((1 << BITS_LEN) - 1) << SHIFT)`),
+    /// it is okay to call [`from_raw()`] with the resulted value and this will give back
+    /// the original value.
+    ///
+    /// [`into_raw()`]: BitFieldCompatible::into_raw
+    /// [`from_raw()`]: BitFieldCompatible::from_raw
     const BITS_LEN: u32;
 
     /// This value must not be overridden! Doing so can cause UB.
     #[doc(hidden)]
     const BITS_MASK: u128 = (1 << Self::BITS_LEN) - 1;
 
-    /// Retrieves the discriminant of the value.
+    /// Retrieves the raw int representation of a value.
     fn into_raw(self) -> u128;
 
-    /// Converts a discriminant into the enum.
+    /// Converts a raw int representation to this type.
     ///
     /// # Safety
     ///
-    /// `v` must be a valid discriminant
+    /// `v` must be a valid value for this type.
     unsafe fn from_raw(v: u128) -> Self;
 }
 
